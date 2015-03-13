4e6f954284fafa8f8072a4080c088d5fd8e5f91d
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-03-13 05:10:27 -0400

    added some better instructions in the admin settings menu

--- pages/main_settings_section_callback.php
+++ pages/main_settings_section_callback.php
@@ -1,3 +1,7 @@
 <?php defined('ABSPATH') or die("Error"); ?>
 
-<p>This is the main configuration section.</p>
+<div style="width:50%;">
+	<p>In the text area below, please enter your Mailchimp popup embed code.</p>
+	<p>If you need to get an embed code, you can get one byogging into your <a href="http://mailchimp.com">Mailchimp Account</a> and either create a new List with a Popup Signup Form or complete the Campaign Wizard</p>
+	<p>At the end of that, you should be presented with a code that you can embed on your websites. Please copy and paste that code into the form below</p>
+</div>
